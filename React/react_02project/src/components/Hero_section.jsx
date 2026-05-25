5f4fdcb571e46620c4cc6c5733521ad186991e32
manishngp <manishngpdoc@gmail.com>
2026-05-25 17:24:10 +0530

    project files initiated

---
+++ React/react_02project/src/components/Hero_section.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar"
+
+
+function Hero_section() {
+
+
+  return (
+   <section>
+    <Navbar />
+
+   </section>
+  )
+}
+
+export default Hero_section
---
+++ React/react_02project/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+function Navbar() {
+  return (
+    <div>
+        <p>TARGET AUDIENCE</p>
+        <p>DIGITAL BANKING PLATFORM</p>
+    </div>
+  )
+}
+
+export default Navbar
