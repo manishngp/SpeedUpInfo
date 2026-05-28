fd61dcaeccd78573683fbc828403488c14ad99d9
manishngp <manishngpdoc@gmail.com>
2026-05-28 11:00:54 +0530

    loder function

---
+++ JavaScript/Second.js
@@ -0,0 +1,12 @@
+// function fun(name, callback){
+//     console.log(`Name is : ${name}`);
+//     callback();
+
+// }
+
+// function cb(){
+//     console.log("hello callback");
+// }
+
+// fun("vikas", cb)
+
