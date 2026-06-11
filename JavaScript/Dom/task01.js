4041ec78af2b22c0d87ac85cb0b1da80fc85f10f
manishngp <manishngpdoc@gmail.com>
2026-06-11 21:52:49 +0530

    on click event in js

---
+++ JavaScript/Dom/task01.js
@@ -0,0 +1,24 @@
+function add(){
+    let a =+ prompt("enter no 1");
+    let b =+ prompt("enter no 2");
+
+    document.write(a+b)
+}
+function sub(){
+    let a =+ prompt("enter no 1");
+    let b =+ prompt("enter no 2");
+
+    document.write(a-b)
+}
+function mul(){
+    let a =+ prompt("enter no 1");
+    let b =+ prompt("enter no 2");
+
+    document.write(a*b)
+}
+function div(){
+    let a =+ prompt("enter no 1");
+    let b =+ prompt("enter no 2");
+
+    document.write(a/b)
+}
