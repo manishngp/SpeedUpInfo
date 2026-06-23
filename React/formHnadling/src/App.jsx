e4f7a76cfcf64bd04ebd165d75b87165d427a7db
manishngp <manishngpdoc@gmail.com>
2026-06-23 11:23:14 +0530

    Form handler in react

--- React/formHnadling/src/App.jsx
+++ React/formHnadling/src/App.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import "./App.css"
 
 function App() {
 
+  const [title, setTitle] = useState("")
+
   const submitHandler =(e)=>{
     e.preventDefault()
-     console.log("form is submitted");
+     
+     console.log("form is submitted by ",title);
+     setTitle("")
   }
 
   return (
    
     <form action="" onSubmit={(e)=>{submitHandler(e)}}>
-      <input type="text" />
+      <input type="text" value={title} onChange={(e)=>{
+        setTitle(e.target.value) 
+         console.log(e.target.value);
+      }} />
       <button>Submit</button>
     </form>
   )
+
+ 
+
 }
 
 export default App
