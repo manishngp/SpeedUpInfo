8859155004ac59a6ce21dd25c3aa1d7823b8bb13
manishngp <manishngpdoc@gmail.com>
2026-06-29 12:18:38 +0530

    framer motion

---
+++ React/Framer Motion/Framer_motion_01/src/App.jsx
@@ -0,0 +1,42 @@
+import { circOut, motion } from 'motion/react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div style={{boxSizing:"border-box"}}>
+      {/* < motion.div
+        style={{ height: "200px", width: "200px", border: "1px solid black", backgroundColor: "grey"}}
+        animate={{ x: "500px", rotate: 720 }}
+        transition={{ duration: 2, delay: 0.5 , repeat:Infinity }}
+      >
+
+      </motion.div>
+      <motion.div
+        style={{ height: "200px", width: "200px", border: "1px solid black", backgroundColor: "yellow" }}
+        animate={{ rotate: 1000 }}
+        transition={{ duration: 2,  repeat:Infinity , ease:"linear" }}
+      >
+
+      </motion.div>
+      < motion.div
+        style={{ height: "200px", width: "200px", border: "1px solid black", backgroundColor: "grey"}}
+        animate={{ x: "-500px", rotate: 720 }}
+        transition={{ duration: 2, delay: 0.5, repeat:Infinity  }}
+      >
+
+      </motion.div>   */}
+      <motion.div
+              style={{ height: "200px", width: "200px", border: "1px solid black", backgroundColor: "green"}}
+              drag
+
+      >
+
+        </motion.div>    
+
+    </div>
+  )
+}
+
+export default App
