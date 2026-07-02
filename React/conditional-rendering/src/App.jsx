04e78a8ff4b63e53d37e612bab5fd60d79b7c920
manishngp <manishngpdoc@gmail.com>
2026-07-02 11:07:09 +0530

    contitions using object mapping

--- React/conditional-rendering/src/App.jsx
+++ React/conditional-rendering/src/App.jsx
@@ -1,15 +1,25 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 
-function App() {
+function App({status}) {
 
     const [notification, setNotification] = useState(0);
     const [user, setUser] = useState("manish");
     const [loading, setLoading] = useState(false);
-    const [error, serError] =useState(false);
+    const [items, setItems]= useState([])
+    
+    const [error, setError] =useState(false);
 
     if(loading) return <h1>Loading... please wait</h1>
     if(error) return <h3>There is some issue</h3>
+
+    const MESSAGE_VIEW={
+      loading : <p>Loading</p>,
+      error :<p>Some error occured </p>,
+      success: <ul>{items.map(item => <li key={item.id}>{item.name}</li>)}</ul>,
+
+    }
 
 
   return (
@@ -21,7 +31,7 @@ function App() {
     {/* using ternary */}
     {notification>0 ? <p>{notification} notification is there.</p> : <p>No notification is there</p>}
 
-     
+     {MESSAGE_VIEW[status]}
     </>
   );
 }
