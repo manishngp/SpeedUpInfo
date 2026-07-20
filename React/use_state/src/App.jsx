9ba410907ca8f22b94f6306311a0db8564eb1ce0
manishngp <manishngpdoc@gmail.com>
2026-07-20 22:28:23 +0530

    useState practice

--- React/use_state/src/App.jsx
+++ React/use_state/src/App.jsx
@@ -1,12 +1,25 @@
-
+import { useEffect, useState } from 'react'
 
 function App() {
+  // const [counter, setCounter]= useState(0);
+  const [bgColor, setBgColor]= useState("red");
+
+useEffect(()=>{
+  setTimeout(() => {
+    setBgColor("blue")
+  },5000);
+},[])
   return (
     <>
-    <pre>
-      thanos
-    </pre>
-    
+
+    <div style={{backgroundColor:bgColor}}>
+      <h2>This is heading</h2>
+    </div>
+    {/* <h2>Counter is {counter}</h2>
+    <button onClick={()=>setCounter(counter+1)}>Add</button>
+    <button  onClick={()=>setCounter(counter-1)}>substract</button>
+    <button  onClick={()=>setCounter(counter*2)}>Multiply</button>
+    <button  onClick={()=>setCounter(counter/2)}>Divide</button> */}
     </>
   )
 }
