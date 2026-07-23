1fbaa824796e8f255932e5a84732c61d0ff1503d
manishngp <manishngpdoc@gmail.com>
2026-07-23 23:30:53 +0530

    routes

---
+++ React/SpeedUp/router/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import {  NavLink } from "react-router-dom";
+function Navbar() {
+  return (
+    <div>
+        <NavLink to="/" >Home</NavLink>
+        <NavLink to="/about" >About</NavLink>
+        <NavLink to="/contact" >Contact</NavLink>
+    </div>
+  )
+}
+
+export default Navbar
